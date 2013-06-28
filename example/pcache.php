ff9cf7a2d7ef6ae128b577f0fd8af90e05866859
Eiji Kitamura <agektmr@gmail.com>
2013-06-28 17:30:41 +0900

    added photos credit

--- example/pcache.php
+++ example/pcache.php
@@ -33,13 +33,6 @@ $d = ($_GET['cm'] == 'pcache' || $_GET['cm'] == null) ? false : true;
     </script>
   </head>
   <body>
-    <div class="navbar">
-      <div class="navbar-inner">
-        <div class="container">
-          <span class="brand">PortableCache.js</span>
-        </div>
-      </div>
-    </div>
     <div class="container">
       <div class="hero-unit">
         <h1 class="hidden-phone">ProtableCache.js</h1>
@@ -49,15 +42,26 @@ $d = ($_GET['cm'] == 'pcache' || $_GET['cm'] == null) ? false : true;
       <div>
         <h2>How to use</h2>
         <p>All you have to do is to insert tags like following:</p>
-        <pre>&lt;meta name="cache-version" content="20130510"&gt;
-&lt;link rel="cachable" href="css/bootstrap.css" type="text/css"&gt;
-&lt;link rel="cachable" href="css/bootstrap-responsive.css" type="text/css"&gt;</pre>
-        <ul>
-          <li>Add cache version as <code>meta</code> tag.</li>
-          <li>Add resource link with <code>rel="cachable"</code>.</li>
-          <li>Load PortableCache.js</li>
-        </ul>
-        <p>Everything else will be taken care of by PortableCache.js.</p>
+        <pre>// declare global cache version using meta[name=”cache-version”]
+&lt;meta name="cache-version" content="20130627"&gt;
+// declare preferred storage to use [file-system, idb, sql, localstorage]
+// without this tag will automatically choose best possible storage
+&lt;meta name="cache-preferred-storage" content="sql"&gt;
+// use DataURL instead of ObjectURL using Blob for Blob resources
+// such as image, audio, video
+&lt;meta name="cache-use-data-url" content="true"&gt;
+// adding “data-cache-src” or “data-cache-href” attribute will
+// declaratively fetch and replace the tag with cache content if possible
+// request declaration.
+&lt;script data-cache-src="js/bootstrap.js" data-cache-version="20130627"&gt;
+&lt;link rel="stylesheet" data-cache-href="css/bootstrap.css" type="text/css"&gt;
+&lt;img data-cache-src="img/121404791_e6a2afda06_b.jpg" /&gt;
+// “data-cache-version” attribute gives resource specific cache version.
+// this enables users to avoid updating whole assets.
+&lt;script data-cache-src="js/bootstrap.js" data-cache-version="20130623"&gt;
+// finally, add PortableCache.js link after scripts declaration.
+&lt;script src="js/PortableCache.js"&gt;&lt;/script&gt;</pre>
+        <p>In declarative mode, everything else will be taken care of by PortableCache.js.</p>
 
         <h2>What does it do?</h2>
         <p>PortableCache.js automatically caches all resources indicated as <code>cachable</code> to the best available storage in user's browser.<br/>
@@ -77,12 +81,13 @@ $d = ($_GET['cm'] == 'pcache' || $_GET['cm'] == null) ? false : true;
         </ul>
         <p>still needs some work on Android legacy browsers.</p>
         <h2>Current Status</h2>
-        <p>Under development. Very early stage.</p>
+        <p>Under development.</p>
         <h2>Example Image Stabs</h2>
-        <img <? if (!$d) { echo 'data-cache-'; } ?>src="img/121404791_e6a2afda06_b.jpg" alt="">
-        <img <? if (!$d) { echo 'data-cache-'; } ?>src="img/3672537740_7a1a3e63fd_b.jpg" alt="">
-        <img <? if (!$d) { echo 'data-cache-'; } ?>src="img/4316514826_81562d1207_b.jpg" alt="">
-        <img <? if (!$d) { echo 'data-cache-'; } ?>src="img/4453502862_e9999a219e_b.jpg" alt="">
+        <p>Photos by <a href="http://www.flickr.com/photos/zb">Gregor Hochmuth</a></p>
+        <img data-cache-src="img/121404791_e6a2afda06_b.jpg" alt="">
+        <img data-cache-src="img/3672537740_7a1a3e63fd_b.jpg" alt="">
+        <img data-cache-src="img/4316514826_81562d1207_b.jpg" alt="">
+        <img data-cache-src="img/4453502862_e9999a219e_b.jpg" alt="">
         <h2>Author</h2>
         <ul>
           <li>Eiji Kitamura (<a href="http://google.com/+agektmr" target="_blank">+agektmr</a>, <a href="http://twitter.com/agektmr" target="_blank">@agektmr</a>)</li>
